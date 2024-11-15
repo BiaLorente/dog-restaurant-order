15c927433b3aba1cfa11481969bff9dc112baa57
beatriz.lorente <beatriz.lorente@ifood.com.br>
2024-11-15 19:27:30 -0300

    Initial commit

---
+++ src/application/pedido.module.ts
@@ -0,0 +1,31 @@
+import { Module } from '@nestjs/common';
+import { OrderController } from './controller/pedido.controller';
+import { IPedidoRepository } from 'src/domain/repositories/order-repository.interface';
+import { IPedidoUseCase } from 'src/domain/use-cases/pedido-use-case.interface';
+import { PedidoUseCase } from 'src/domain/use-cases/pedido-use-case.service';
+import { databaseProviders } from 'src/infrastructure/data/database.provider';
+import { pedidoProviders } from 'src/infrastructure/data/repositories/pedido.provider';
+import { PedidoRepository } from 'src/infrastructure/data/repositories/pedido.repository';
+import { OrderMapper } from './mapper/pedido.mapper';
+import { ProdutoModule } from 'src/infrastructure/client/produto/produto.module';
+
+@Module({
+  imports: [ProdutoModule],
+  controllers: [OrderController],
+  providers: [
+    OrderMapper,
+    ...pedidoProviders,
+    ...databaseProviders,
+    PedidoUseCase,
+    {
+      provide: IPedidoUseCase,
+      useClass: PedidoUseCase,
+    },
+    PedidoRepository,
+    {
+      provide: IPedidoRepository,
+      useClass: PedidoRepository,
+    },
+  ],
+})
+export class PedidoModule {}
